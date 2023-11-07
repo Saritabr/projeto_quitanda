adeb15c18b12c8dedf0822e62ae08fac240499cf
Sara <sara.resende.senai@gmail.com>
2023-11-07 15:59:32 -0300

    mais python

--- quitanda.py
+++ quitanda.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_tamplate, request, redirect, session
+from flask import Flask, render_template, request, redirect, session
 import sqlite3 as sql
 import uuid
 
@@ -20,7 +20,7 @@ def verificar_sessao():
 def conecta_database():
     conexao = sql.connect("db_quitanda.db")
     conexao.row_factory = sql.Row
-    return conexao 
+    return conexao
 
 #INICIAR O BANCO DE DADOS
 def iniciar_db():
@@ -30,15 +30,38 @@ def iniciar_db():
     conexao.commit()
     conexao.close()
 
-#ROTA DA PÁGINA INICIAL- Definir iniciar_db inicializar esquema.sql
-@app.route('/')
+# ROTA DA PÁGINA INICIAL
+@app.route("/")
 def index():
-    iniciar_db() #chamando o BD
+    iniciar_db()
     conexao = conecta_database()
     produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
     conexao.close()
     title = "Home"
     return render_template("home.html", produtos=produtos, title=title)
 
+#ROTA DA PÁGINA LOGIN
+@app.route("/login")
+def login():
+    title="Login"
+    return render_template("login.html", title=title)
+
+# ROTA DA PÁGINA ACESSO
+@app.route("/acesso", methods=['post'])
+def acesso():
+
+global usuario, senha
+usuario_informado = request.form["usuario"]
+senha_informada = request.form["senha"]
+if usuario == usuario_informado and senha ==
+
+senha_informada:
+
+session["login"] = True
+return redirect('/adm')
+
+else: return 
+render_template("login.html",msg="Usuário/Senha estão
+incorretos!")
 #FINAL DO CÓDIGO
 app.run(debug=True)
