c8d8038e7a4f28134f0687cf3dbffbcbec36a8a3
Sara <sara.resende.senai@gmail.com>
2023-11-17 13:22:56 -0300

    commit

--- quitanda.py
+++ quitanda.py
@@ -49,19 +49,114 @@ def login():
 # ROTA DA PÁGINA ACESSO
 @app.route("/acesso", methods=['post'])
 def acesso():
+    global usuario, senha
+    usuario_informado = request.form["usuario"]
+    senha_informada = request.form["senha"]
+    if usuario == usuario_informado and senha == senha_informada:
+        session["login"] = True
+        return redirect('/adm')
+    else: 
+        return render_template("login.html",msg="Usuário/Senha estão incorretos!")
 
-global usuario, senha
-usuario_informado = request.form["usuario"]
-senha_informada = request.form["senha"]
-if usuario == usuario_informado and senha ==
+# ROTA DA PÁGINA ADM 
+@app.route("/adm")
+def adm():
+    if verificar_sessao():
+        iniciar_db()
+        conexao = conecta_database()
+        produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
+        conexao.close()
+        title="Administração"
+        return render_template("adm.html", produtos=produtos, title=title)
+    else:
+        return redirect("/login")
+    
+#código do LOGOUT
+@app.route("/logout")
+def logout():
+    global login
+    login = False
+    session.clear()
+    return redirect('/')
 
-senha_informada:
+#ROTA DA PÁGINA DE CADASTRO
+@app.route("/cadastro")
+def cadastro():
+    if verificar_sessao():
+        title = "Cadastro de produtos"
+        return render_template("cadastro.html", title=title)
+    else:
+        return redirect("/login")
 
-session["login"] = True
-return redirect('/adm')
+# ROTA DA PÁGINA DE CADASTRO NO BANCO 
+@app.route("/prodcadastro", methods=["post"])
+def prodcadastro():
+    if verificar_sessao():
+        nome_prod=request.form['nome_prod']
+        desc_prod=request.form['desc_prod']
+        preco_prod=request.form['preco_prod']
+        img_prod=request.files['img_prod']
+        id_foto=str(uuid.uuid4().hex)
+        filename=id_foto+nome_prod+'.png'
+        img_prod.save("static/img/produtos/"+filename)
+        conexao = conecta_database()
+        conexao.execute('INSERT INTO produtos (nome_prod, desc_prod, preco_prod, img_prod) VALUES (?, ?, ?, ?)', (nome_prod, desc_prod, preco_prod, filename))
+        conexao.commit()
+        conexao.close()
+        return redirect("/adm")
+    else:
+        return redirect("/login")
+    
+#ROTA DE EXCLUSÃO
+@app.route("/excluir/<id>")
+def excluir(id):
+    if verificar_sessao():
+        id = int(id)
+        conexao = conecta_database()
+        conexao.execute('DELETE FROM produtos WHERE id_prod = ?', (id,))
+        conexao.commit()
+        conexao.close()
+        return redirect('/adm')
+    else:
+        return redirect("/login")
+    
+#CRIAR A ROTA DO EDITAR
+@app.route("/editprodutos/<id_prod>")
+def editar (id_prod):
+    if verificar_sessao():
+        iniciar_db()
+        conexao = conecta_database()
+        produtos = conexao.execute('SELECT * FROM produtos WHERE id_prod = ?', (id_prod,)).fetchall()
+        conexao.close()
+        title="Edição de produtos"
+        return render_template("editprodutos.html", produtos=produtos, title=title)
+    else:
+        return redirect("/login")
 
-else: return 
-render_template("login.html",msg="Usuário/Senha estão
-incorretos!")
+#CRIAR A ROTA PARA TRATAR A EDIÇÃO
+@app.route("/editarprodutos", methods=['POST'])
+def editprod():
+    id_prod = request.form['id_prod']
+    nome_prod=request.form['nome_prod']
+    desc_prod=request.form['desc_prod']
+    preco_prod=request.form['preco_prod']
+    img_prod=request.files ['img_prod']
+    id_foto=str(uuid.uuid4().hex)
+    filename=id_foto+nome_prod+'.png'
+    img_prod.save("static/img/produtos/"+filename)
+    conexao = conecta_database()
+    conexao.execute( 'UPDATE produtos SET nome_prod = ?, desc_prod = ?, preco_prod = ?, img_prod = ? WHERE id_prod = ?', (nome_prod, desc_prod, preco_prod, filename,id_prod))
+    conexao.commit()
+    conexao.close()
+    return redirect('/adm')
+
+# ROTA DA PÁGINA DE BUSCA 
+@app.route("/busca", methods=["post"])
+def busca():
+    busca=request.form['buscar']
+    conexao = conecta_database()
+    produtos=conexao.execute('SELECT * FROM produtos WHERE nome_prod LIKE "%" || ? || "%"', (busca,)).fetchall()
+    title="Home"
+    return render_template("home.html", produtos=produtos, title=title)
 #FINAL DO CÓDIGO
 app.run(debug=True)
